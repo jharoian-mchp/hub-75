3ebe60c72df940d7a76aaed4a0c4f9aefa71b845
Darren Wenn <darren.wenn@microchip.com>
2024-05-23 11:46:35 +0100

    Updated fb_image to handle multiple screens

--- software/fb_image.c
+++ software/fb_image.c
@@ -9,6 +9,7 @@
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <argp.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <unistd.h>
@@ -19,27 +20,70 @@ int  mem_fd;    // used only in setup_io
 void *cape_map; // used only in setup_io
 volatile unsigned *cape_starting_addr; // set in setup_io
 
+struct arguments {
+  char *s_input_file;
+  int screen_num;
+};
+
+static struct argp_option options[] = {
+  {"screen", 's', "SCREEN", 0, "Write the image to the specified screen [0-7], default is 1"},
+  {"file", 'f', "FILE", 0, "Input file"},
+  {0}
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+  struct arguments *arguments = state->input;
+
+  switch (key) {
+    case 's':
+      arguments->screen_num = atoi(arg);
+      if (arguments->screen_num < 0 || arguments->screen_num > 7) {
+        argp_failure(state, 1, 0, "Screen number must be between 0 and 7");
+      }
+      break;
+    case 'f':
+      arguments->s_input_file = arg;
+      break;
+    case ARGP_KEY_ARG:
+      if (arguments->s_input_file != NULL) {
+        argp_failure(state, 1, 0, "Filename specified multiple times");
+      }
+      arguments->s_input_file = arg;
+      break;
+    case ARGP_KEY_END:
+      if (arguments->s_input_file == NULL) {
+        argp_failure(state, 1, 0, "Input filename not specified");
+      }
+      break;
+    default:
+      return ARGP_ERR_UNKNOWN;
+  }
+  return 0;
+}
+
+static struct argp argp = {options, parse_opt, 0, 0};
+
 void setup_io();
 
 int main(int argc, char **argv)
 {
-  FILE *file;
-  const char *filename;
-  int *array;
   uint32_t *pixel_array;
   uint32_t pixel;
-  size_t num_elements;
-  int x, y;
-
-  if (argc != 2) {
-      fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-      return EXIT_FAILURE;
-  }
+  int x, y, x_offset;
+  struct arguments arguments;
+  arguments.s_input_file = NULL;
+  arguments.screen_num = 0;
 
   // Set up gpi pointer for direct register access
   setup_io();
 
-  const char *input_image = argv[1];
+  argp_parse(&argp, argc, argv, 0, 0, &arguments);
+  if (arguments.s_input_file == NULL) {
+    return EXIT_FAILURE;
+  }
+
+  x_offset = arguments.screen_num * 64;
 
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
     fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
@@ -51,7 +95,7 @@ int main(int argc, char **argv)
     return EXIT_FAILURE;
   }
 
-  SDL_Surface *image = IMG_Load(input_image);
+  SDL_Surface *image = IMG_Load(arguments.s_input_file);
   if (!image) {
     fprintf(stderr, "IMG_Load Error: %s\n", IMG_GetError());
     SDL_Quit();
@@ -74,7 +118,7 @@ int main(int argc, char **argv)
   for(y = 0; y < 64; y++) {
       for(x = 0; x < 64; x++) {
         pixel = pixel_array[x + (y * 64)];
-        cape_starting_addr[x + (y * 512)] = pixel;
+        cape_starting_addr[x + (y * 512) + x_offset] = pixel;
       }
   }
 
